a9efd80c42571c8e9dd879d2ce4c798f07bcd7e6
Gregorio Nicolas Gerardi <gregorionicolasgerardi@gmail.com>
2018-11-12 12:12:44 -0300

    skeleton for project

---
+++ app/src/main/java/com/example/gregoriogerardi/fittrackerkotlin/FirstAppScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gregoriogerardi.fittrackerkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FirstAppScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app_screen)
+    }
+}
